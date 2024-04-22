52a88e989a2501bb733034fba07462950fdd6c5d
5uy45h <suyash@easyecom.io>
2024-04-22 10:07:06 +0530

    First commit making css

---
+++ client/client-blog/src/App.js
@@ -0,0 +1,28 @@
+import './App.css';
+
+function App() {
+  return (
+    <main>
+      <header>
+        <a href="" className='logo'>MyBlog</a>
+        <nav>
+          <a href="">Login</a>
+          <a href="">Register</a>
+        </nav>
+      </header>
+      <div className="post">
+        <div className="image">
+        <img src="https://cdn.vectorstock.com/i/1000x1000/79/80/blue-background-with-desktop-computer-and-link-vector-15127980.webp"></img>
+        </div>
+        <div className="texts">
+        <h2>First Blog</h2>
+        <p>This is the paragraph for the given blog</p>
+      </div>
+        
+      </div>
+    </main>
+  )
+  
+}
+
+export default App;
