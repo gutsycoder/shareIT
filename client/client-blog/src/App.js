bab8b81a395f84c9d3a0af20646bb5487b9ea49c
gutsycoder <suyashsincever13@gmail.com>
2024-05-06 01:46:04 +0530

    Creating a new post

--- client/client-blog/src/App.js
+++ client/client-blog/src/App.js
@@ -7,6 +7,7 @@ import IndexPage from './pages/IndexPage';
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
 import { UserContextProvider } from './UserContext';
+import CreatePost from './pages/CreatePost';
 function App() {
   return (
     <UserContextProvider>
@@ -18,6 +19,7 @@ function App() {
         } />
         <Route path='/login' element={<LoginPage/>}></Route>
         <Route path='/register' element={<RegisterPage/>}></Route>
+        <Route path="/create" element={<CreatePost/>}></Route>
       </Route>
     
     </Routes>
